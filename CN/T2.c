5e7d062097690f1886ef92258b3e5ae97cb12252
Igor Nunes <101185370+igornunes7@users.noreply.github.com>
2023-05-24 09:43:48 -0400

    Update T2.c

--- CN/T2.c
+++ CN/T2.c
@@ -5,12 +5,12 @@
 #include <stdbool.h>
 #include <time.h>
 
-void gaussJacobi(int ordem, float matriz[ordem][ordem], float termo_ind[ordem], float sol_inicial[ordem], float erro) {
+void gaussJacobi(int ordem, double matriz[ordem][ordem], double termo_ind[ordem], double sol_inicial[ordem], double erro) {
     int i, j, cont = 0;
     bool aux = true;
     clock_t inicio, fim;
 
-    float x[ordem], // nova soluções
+    double x[ordem], // nova soluções
     x_ant[ordem], // solução anterior
     dif;
 
@@ -18,7 +18,7 @@ void gaussJacobi(int ordem, float matriz[ordem][ordem], float termo_ind[ordem], 
     printf("\nIteracao %d:\n", cont);
     printf("Solucao Inicial:\n");
     for (i = 0; i < ordem; i++) {
-        printf("x%d = %f\n", i + 1, sol_inicial[i]);
+        printf("x%d = %.7lf\n", i + 1, sol_inicial[i]);
     }
 
     while (aux) {
@@ -40,7 +40,7 @@ void gaussJacobi(int ordem, float matriz[ordem][ordem], float termo_ind[ordem], 
         printf("\nIteracao %d:\n", cont);
         printf("Solucao:\n");
         for (i = 0; i < ordem; i++) {
-            printf("x%d = %f\n", i + 1, x[i]);
+            printf("x%d = %.7lf\n", i + 1, x[i]);
         }
 
         dif = 0.0;
@@ -56,12 +56,12 @@ void gaussJacobi(int ordem, float matriz[ordem][ordem], float termo_ind[ordem], 
 
     printf("\nSolucao final:\n");
     for (i = 0; i < ordem; i++) {
-        printf("x%d = %f\n", i + 1, x[i]);
+        printf("x%d = %.7lf\n", i + 1, x[i]);
     }
     fim = clock();
 
 
-    float tempo_gasto = ((float) (fim - inicio)) / CLOCKS_PER_SEC;
+    double tempo_gasto = ((double) (fim - inicio)) / CLOCKS_PER_SEC;
 
     printf("Tempo gasto: %.7lf segundos\n", tempo_gasto);
 }
@@ -71,7 +71,7 @@ int main() {
     printf("Ordem da matriz: ");
     scanf("%d", &ordem);
 
-    float matriz[ordem][ordem], termo_ind[ordem], sol_inicial[ordem], erro;
+    double matriz[ordem][ordem], termo_ind[ordem], sol_inicial[ordem], erro;
 
 
     //Pedindo os elementos do sistema
@@ -79,27 +79,27 @@ int main() {
     for (i = 0; i < ordem; i++) {
         for (j = 0; j < ordem; j++) {
             printf("Matriz[%d][%d]: ", i + 1, j + 1);
-            scanf("%f", &matriz[i][j]);
+            scanf("%lf", &matriz[i][j]);
         }
     }
 
     //Pedindo os termos independentes
     printf("Digite os termos independentes:\n");
     for (i = 0; i < ordem; i++) {
-        scanf("%f", &termo_ind[i]);
+        scanf("%lf", &termo_ind[i]);
     }
 
 
     //Pedindo a solução inicial
     printf("Digite a solucao inicial:\n");
     for (i = 0; i < ordem; i++) {
-        scanf("%f", &sol_inicial[i]);
+        scanf("%lf", &sol_inicial[i]);
     }
 
 
     //Pedindo o erro
     printf("Digite o erro:\n");
-    scanf("%f", &erro);
+    scanf("%lf", &erro);
 
 
 
@@ -107,7 +107,7 @@ int main() {
     printf("\nO sistema digitado foi:\n");
     for(i = 0; i < ordem; i++) {
         for(j = 0; j < ordem; j++) {
-            printf("%0.7f ", matriz[i][j]);
+            printf("%.7lf ", matriz[i][j]);
         }
         printf("\n");
     }
@@ -116,19 +116,19 @@ int main() {
     //Imprimindo os termos independentes
     printf ("\nTermos independentes:\n");
     for (i = 0; i < ordem; i++) {
-        printf ("%0.7f\n" , termo_ind[i]);
+        printf ("%.7lf\n" , termo_ind[i]);
     }
 
 
     //Imprimindo a solução inicial
     printf ("\nSolucao Inicial:\n");
     for (i = 0; i < ordem; i++) {
-        printf ("%0.7f\n" , sol_inicial[i]);
+        printf ("%.7lf\n" , sol_inicial[i]);
     }
 
 
     //Imprimindo o erro
-    printf ("\nErro = %0.7f\n" , erro);
+    printf ("\nErro = %.7lf\n" , erro);
 
 
     gaussJacobi(ordem, matriz, termo_ind, sol_inicial, erro);
